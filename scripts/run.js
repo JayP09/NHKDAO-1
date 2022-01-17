71173dafe0e9b477002d97375c7c5150d99dba54
Jay Panchal <panchalj272@gmail.com>
2022-01-17 21:56:07 +0530

    Updated scripts file

--- scripts/run.js
+++ scripts/run.js
@@ -12,39 +12,31 @@ async function main() {
   const NhkTokenContract = await NhkToken.deploy();
   await NhkTokenContract.deployed();
 
-  console.log("NhkToken Address", NhkTokenContract.address);
-
-  let totalSupply = (await NhkTokenContract.balanceOf(owner.address)).toNumber();
+  let totalSupply = parseInt(await ethers.utils.formatEther(await NhkTokenContract.balanceOf(owner.address)));
   console.log("Nhk total Supply",totalSupply);
+  const claimSupply = parseInt((totalSupply * 30)/100);
+  const burnSupply = parseInt((totalSupply * 25)/100);
 
   const NhkTokenBurn = await hre.ethers.getContractFactory("Burn");
   const NhkTokenBurnContract = await NhkTokenBurn.deploy(NhkTokenContract.address);
   await NhkTokenBurnContract.deployed();
 
-  NhkTokenContract.transfer(NhkTokenBurnContract.address,totalSupply/2);
-  console.log("token for burn",(await NhkTokenContract.balanceOf(NhkTokenBurnContract.address)).toNumber());
+  NhkTokenContract.transfer(NhkTokenBurnContract.address,ethers.utils.parseEther(burnSupply.toString()));
+  console.log("token for burn",parseInt(await ethers.utils.formatEther(await NhkTokenContract.balanceOf(NhkTokenBurnContract.address))));
 
-  console.log("Owner Balance",(await NhkTokenContract.balanceOf(owner.address)).toNumber());
+  console.log("Owner Balance",parseInt(await ethers.utils.formatEther(await NhkTokenContract.balanceOf(owner.address))));
 
   const NhkTokenClaim = await hre.ethers.getContractFactory("Claim");
   const NhkTokenClaimContract = await NhkTokenClaim.deploy(NhkTokenContract.address);
   await NhkTokenClaimContract.deployed();
 
-  NhkTokenContract.transfer(NhkTokenClaimContract.address,totalSupply/2);
-  console.log("token for Claim",(await NhkTokenContract.balanceOf(NhkTokenClaimContract.address)).toNumber());
+  NhkTokenContract.transfer(NhkTokenClaimContract.address,ethers.utils.parseEther(claimSupply.toString()));
+  console.log("token for Claim",parseInt(await ethers.utils.formatEther(await NhkTokenContract.balanceOf(NhkTokenClaimContract.address))));
 
-  let claimTxn1 = await NhkTokenClaimContract.connect(user1).claim();
-  await claimTxn1.wait();
-
-//   console.log("Claim Transaction",claimTxn1);
-  console.log("User1 Balance",(await NhkTokenContract.balanceOf(user1.address)).toNumber());
-
-  let burnTxn1 = await NhkTokenBurnContract.connect(user1).burn(5);
-  await burnTxn1.wait();
-
-//   console.log("Burn Transaction",burnTxn1);
-  console.log("User1 Balance",(await NhkTokenContract.balanceOf(user1.address)).toNumber());
-//   console.log("Burn Address Token",(await NhkTokenContract.balanceOf(address(0))).toNumber())
+  console.log("NhkToken Address", NhkTokenContract.address);
+  console.log("ClaimContract Address",NhkTokenClaimContract.address);
+  console.log("BurnContract", NhkTokenBurnContract.address);
+  console.log("owner Address",owner.address)
 }
 
 
